474f721b90d4363b846f1371a8ff9281b6e326ca
swathivs <swathivs1406@gmail.com>
2023-05-16 11:07:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+const app = express();
+import fs from 'fs';
+
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/create', (request, response) => {
+  var content = new Date().getTime().toLocaleString();
+
+  var dateTime = `${new Date().getDate()}-${
+    new Date().getMonth() + 1
+  }-${new Date().getFullYear()}-${new Date().getHours()}-${new Date().getMinutes()}-${new Date().getSeconds()}`;
+  //   console.log(dateTime);
+  fs.writeFile(`./${dateTime}.txt`, content, (err) => {
+    if (err) console.log(err);
+    else console.log('success');
+  });
+  response.send('files created');
+});
+
+//Task-2 to retrieve the files from the Particular folder
+
+app.get('/files', (request, response) => {
+  fs.readdir('./', (err, Files) => {
+    if (err) console.log(err);
+    else response.json({ Files });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`The Server Started at the port :  ${port}`);
+});
